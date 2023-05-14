9bd72bcc0ae63c0f1c99960c1b504e1f8d9b3200
David Jimenez Belmonte <djimenezib@edu.tecnocampus.cat>
2023-05-14 16:32:32 +0200

    CPV Routes fixes

--- routes/cpvRoutes.js
+++ routes/cpvRoutes.js
@@ -3,17 +3,14 @@ const cpvController = require('./../controllers/cpvController');
 
 const router = express.Router();
 
-// router
-//   .route("/")
-//   .get(cpvController.getAll)
-//   .post(cpvController.create);
+router.route('/').get(cpvController.getAll).post(cpvController.create);
 
-router.route('/search').get(cpvController.search);
+// router.route('/search').get(cpvController.search);
 
-// router
-//   .route("/:id")
-//   .get(cpvController.get)
-//   .patch(cpvController.update)
-//   .delete(cpvController.delete);
+router
+  .route('/:id')
+  .get(cpvController.get)
+  .patch(cpvController.update)
+  .delete(cpvController.delete);
 
 module.exports = router;
