e375d1f220cf3d120e5f435157df918cda7ce7e2
David Jimenez Belmonte <djimenezib@edu.tecnocampus.cat>
2023-05-21 17:52:56 +0200

    password encrypt + nodemon update

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -26,6 +27,13 @@ const userSchema = new mongoose.Schema({
     },
     default: 'user',
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
