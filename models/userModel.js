a29600b00bab10807173b0648c446c9623100186
David Jimenez Belmonte <djimenezib@edu.tecnocampus.cat>
2023-05-21 19:27:51 +0200

    add login

--- models/userModel.js
+++ models/userModel.js
@@ -18,6 +18,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minlength: 8,
+    select: false,
   },
   role: {
     type: String,
@@ -35,6 +36,13 @@ userSchema.pre('save', async function (next) {
   this.password = await bcrypt.hash(this.password, 12);
   next();
 });
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
